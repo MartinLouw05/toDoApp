2aa7690b68fef64c9b71c9791480994a8157b410
Martin Louw <louw.martin05@gmail.com>
2022-07-10 13:36:48 +0200

    Added more navigation functionality
    Added functionality to change the displayed list when clicking on the buttons.
    Excluding buttons that are within the list display.

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -2,6 +2,10 @@
 const topButton = document.getElementById("topButton");
 const bottomButton = document.getElementById("bottomButton");
 
+const todaysTasks = document.getElementsByClassName("displayTodaysTasks");
+const allTasks = document.getElementsByClassName("displayAllTasks");
+const createTask = document.getElementsByClassName("createTask");
+
 topButton.addEventListener('click', (e) => {
     if (topButton.className == "btnToday" && bottomButton.className == "btnCreateBottom") {
         topButton.className = "btnCreateTop";
@@ -11,16 +15,25 @@ topButton.addEventListener('click', (e) => {
         bottomButton.className = "btnViewTasks";
         bottomButton.innerHTML = "<img src=\"/images/to-do-list-icon-buy-this-icon-for--0-48-1.png\" alt=\"Create Task Image Not Found\" class=\"btnImage\"><br>";
         bottomButton.innerHTML += "View All Tasks";
+
+        changeDisplay(allTasks, 'none');
+        changeDisplay(todaysTasks, 'block');
     }
     else if (topButton.className == "btnCreateTop") {
         topButton.className = "btnToday";
         topButton.innerHTML = "<img src=\"/images/to-do-list-icon-buy-this-icon-for--0-48-1.png\" alt=\"Create Task Image Not Found\" class=\"btnImage\"><br>";
         topButton.innerHTML += "Today's Tasks";
+
+        changeDisplay(todaysTasks, 'none');
+        changeDisplay(createTask, 'block');
     }
     else if (topButton.className == "btnToday") {
         topButton.className = "btnCreateTop";
         topButton.innerHTML = "<img src=\"/images/to-do-list-icon-buy-this-icon-for--0-48-1.png\" alt=\"Create Task Image Not Found\" class=\"btnImage\"><br>";
         topButton.innerHTML += "Create New Task";
+
+        changeDisplay(createTask, 'none');
+        changeDisplay(todaysTasks, 'block');
     }
     else {
         console.log("ERROR. Something Went Wrong With The Top Button");
@@ -36,13 +49,28 @@ bottomButton.addEventListener('click', (e) => {
         topButton.className = "btnToday";
         topButton.innerHTML = "<img src=\"/images/to-do-list-icon-buy-this-icon-for--0-48-1.png\" alt=\"Create Task Image Not Found\" class=\"btnImage\"><br>";
         topButton.innerHTML += "Today's Tasks";
+
+        changeDisplay(todaysTasks, 'none');
+        changeDisplay(allTasks, 'block');
     }
     else if (bottomButton.className == "btnCreateBottom") {
         bottomButton.className = "btnViewTasks";
         bottomButton.innerHTML = "<img src=\"/images/to-do-list-icon-buy-this-icon-for--0-48-1.png\" alt=\"Create Task Image Not Found\" class=\"btnImage\"><br>";
         bottomButton.innerHTML += "View All Tasks";
+
+        changeDisplay(allTasks, 'none');
+        changeDisplay(createTask, 'block');
     }
     else {
         console.log("ERROR. Something Went Wrong With The Bottom Button");
     }
 });
+
+function changeDisplay(tab, display) {
+    let i;
+    let tabLength = tab.length;
+
+    for(i = 0; i < tabLength; i++) {
+        tab[i].style["display"] = display;
+    }
+};
