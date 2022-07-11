c0f9232f0863c2020694a19b717ce25194536304
Martin Louw <louw.martin05@gmail.com>
2022-07-11 14:15:03 +0200

    Added and Edited Functionality
    Added the full list display functionality.
    Start with "strike-through" functionality, but is giving an ERROR (returning WINDOW and not the ELEMENT).
    Started with the Task Status, but it is incomplete and not fully functional.

--- javascript/tasks.js
+++ javascript/tasks.js
@@ -46,11 +46,11 @@ class Tasks {
     }
 
     //Create Task
-    createTask(taskName, taskDescription, taskPriority, taskDate, taskTime) {
+    createTask(newTask) {
         console.log("You are now creating a Task");
-        let newTask = [ taskName, taskDescription, taskPriority, taskDate, taskTime ];
         this._toDoList.push(newTask);
         console.log(data);
+        completeTaskCreation();
     }
 }
 
@@ -67,17 +67,110 @@ sumbitTask.addEventListener('click', (e) => {
     let taskDate = taskDateInput.value;
     let taskTime = taskTimeInput.value;
 
-    addTask.createTask(taskName, taskDescription, taskPriority, taskDate, taskTime);
+    let taskObj = { objTaskName : taskName, objTaskDescription : taskDescription, objTaskPriority : taskPriority, objTaskDate : taskDate, objTaskTime : taskTime };
+
+    addTask.createTask(taskObj);
     console.log("Task Data Submitted");    
 });
+
+function completeTaskCreation() {
+    alert("New Task Succesfully Created");
+
+    const allTasksButton = document.getElementById("bottomButton");
+    allTasksButton.click();
+}
 
 //Edit Task
 
 //Delete Task
 
 //Complete Task
+class Completion extends Tasks {
+    constructor(taskName, taskDescription, taskPriority, taskDate, taskTime, taskStatus) {
+        super(taskName, taskDescription, taskPriority, taskDate, taskTime);
+        this._taskStatus = taskStatus;
+    }
+    get taskStatus() {
+        return this._taskStatus;
+    }
+    set taskStatus(text) {
+        if (isNaN(text)) {
+            this._taskStatus = text;
+        }
+        else {
+            console.log('You must assign a string value to your task status');
+        }
+    }
+
+    changeTaskStatus(taskStatus) {
+        console.log("You are now creating a task status");
+        if (taskStatus == "completed") {
+            taskStatus = "uncompleted";
+            this._toDoList.push(taskStatus);
+        }
+    }
+}
+
+function changeStatus() {
+    let selectedEntry = document.getElementsByClassName("allTasksEntries");
+    let length = selectedEntry.length;
+
+    for (i = 0; i < length; i++) {
+        selectedEntry[i].addEventListener('dblclick', (e) => {     
+            //ERROR RETURNING WINDOW AND NOT ELEMENT
+            let select = this.id;
+            document.getElementById(select).style.textDecoration = "line-through";
+        })
+    }
+}
 
 //Create Today's Task List
 
 //Create Full Task List
+const viewAllTasks = document.getElementById("bottomButton");
+
+viewAllTasks.addEventListener('click', (e) => {
+    if (viewAllTasks.className == "btnCreateBottom") {
+        createAllTasksList(addTask.toDoList);
+    }
+    else {
+        console.log("Something went wrong");
+    }
+});
+
+function createAllTasksList(dataArray) {
+    arrayLength = dataArray.length;
+
+    for (i = 0; i < arrayLength; i++) {
+        console.log("You are creating the ALL TASKS LIST");
+
+        const newListEntry = document.createElement('li');
+        newListEntry.className = "allTasksEntries";
+        newListEntry.id = i;
+
+        let entriesArray = [];
+
+        entriesArray.push(dataArray[i].objTaskName);
+        entriesArray.push(dataArray[i].objTaskDescription);
+        entriesArray.push(dataArray[i].objTaskPriority);
+        entriesArray.push(dataArray[i].objTaskDate);
+        entriesArray.push(dataArray[i].objTaskTime);
+
+        let entriesArrayLength = entriesArray.length;
+
+        for (x = 0; x < entriesArrayLength; x++) {
+            const listData = document.createElement('span');
+            let listEntry = document.createTextNode(entriesArray[x]);
+            listData.appendChild(listEntry);
+
+            newListEntry.append(listData);
+        }
+
+        const allTasksList = document.getElementById("allTasksList");
+        allTasksList.append(newListEntry);
+
+        changeStatus();
+    }
+}
+
 
